1e9c1cae6cde99d86bd258657a1ad229b940f9b8
Phillip <phv9495@saintjoe.edu>
2015-02-09 14:29:58 -0500

    Typed in the three queries from the homework as comments.

---
+++ ch4Homework.js
@@ -0,0 +1,10 @@
+//Phil Hofer
+//2/9/15
+//ch4Homework.js
+//These are the three queries for the homework.
+
+//select distinct fname, lname from employee, works_on where dno = 5 and ssn = essn and pno = 1 and hours > 10;
+//
+//select fname, lname from employee, dependent where dependent where dependent_name = fname;
+//
+//select fname, lname from employee, department where super_ssn = mgr_ssn;
